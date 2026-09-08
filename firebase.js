2f6c55f431df2a9addd90eee2850c0fb8ba960f4
aksomosan-cloud <aksomosan-cloud@users.noreply.github.com>
2026-09-08 20:50:40 +0300

    update code

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore, collection, addDoc, onSnapshot, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC21VVoQavyr0TNVBQTyXCZkIhgYdemvVg",
+  authDomain: "ahmadskh-6d7a1.firebaseapp.com",
+  projectId: "ahmadskh-6d7a1",
+  storageBucket: "ahmadskh-6d7a1.firebasestorage.app",
+  messagingSenderId: "55483296072",
+  appId: "1:55483296072:web:51ee44b5dc5fcc8b5eeb7d",
+  measurementId: "G-L4PLEJJ8E1"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export { collection, addDoc, onSnapshot, serverTimestamp };
